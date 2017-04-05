2b82d1b6c20f10cb0e9d55da7e07d814d42e6690
Julian Currie <julian.currie@gmail.com>
2017-04-05 20:38:55 +0700

    Made router and pkg changes for react-router 4, added Login

--- client/main.js
+++ client/main.js
@@ -1,22 +1,21 @@
 import { Meteor } from "meteor/meteor";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route } from "react-router";
-import createBrowserHistory from "history/createBrowserHistory";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Login from "../imports/ui/Login";
 import Signup from "../imports/ui/Signup";
 import Link from "../imports/ui/Link";
 import NotFound from "../imports/ui/NotFound";
 
-const browserHistory = createBrowserHistory();
-
 const routes = (
-  <Router history={browserHistory}>
-    <div>
-      <Route path="/signup" component={Signup} />
-      <Route path="/link" component={Link} />
-      <Route path="*" component={NotFound} />
-    </div>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/links" component={Link} />
+      <Route component={NotFound} />
+    </Switch>
   </Router>
 );
 
