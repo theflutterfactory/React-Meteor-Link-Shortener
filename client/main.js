9bd83e23eeb12743420e356ec63864f98a435952
Julian Currie <julian.currie@gmail.com>
2017-04-05 20:04:42 +0700

    Added Signup, Link, and new NotFound route

--- client/main.js
+++ client/main.js
@@ -1,9 +1,25 @@
 import { Meteor } from "meteor/meteor";
 import React from "react";
 import ReactDOM from "react-dom";
-import Signup from "./../imports/ui/Signup";
-import Link from "./../imports/ui/Link";
+import { Router, Route } from "react-router";
+import createBrowserHistory from "history/createBrowserHistory";
+
+import Signup from "../imports/ui/Signup";
+import Link from "../imports/ui/Link";
+import NotFound from "../imports/ui/NotFound";
+
+const browserHistory = createBrowserHistory();
+
+const routes = (
+  <Router history={browserHistory}>
+    <div>
+      <Route path="/signup" component={Signup} />
+      <Route path="/link" component={Link} />
+      <Route path="*" component={NotFound} />
+    </div>
+  </Router>
+);
 
 Meteor.startup(() => {
-  ReactDOM.render(<Link />, document.getElementById("app"));
+  ReactDOM.render(routes, document.getElementById("app"));
 });
