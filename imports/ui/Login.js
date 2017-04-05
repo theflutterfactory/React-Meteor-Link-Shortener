d72e4dde54faf7166a2370fe4f6a67c277790bed
Julian Currie <julian.currie@gmail.com>
2017-04-05 21:24:32 +0700

    Added html and links between Login and Signup screens

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class Login extends React.Component {
   render() {
-    return <p>Login Component</p>;
+    return (
+      <div>
+        <h1>Login to Link Shortener</h1>
+        <p>Login Form</p>
+        <Link to="/signup">Don't have an account? Signup</Link>
+      </div>
+    );
   }
 }
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class Signup extends React.Component {
   render() {
-    return <p>Signup Component</p>;
+    return (
+      <div>
+        <h1>Sign up for Link Shortener</h1>
+        <p>Sign up form</p>
+        <Link to="/">Have an account?</Link>
+      </div>
+    );
   }
 }
