49933a2eab6d98a61f348825faaae1bd4314dad1
Julian Currie <julian.currie@gmail.com>
2017-04-06 15:35:44 +0700

    added generic Signup form with generic error msg

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,11 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ""
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.setState({
+      error: "Opps..Something went wrong."
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Sign up for Link Shortener</h1>
-        <p>Sign up form</p>
+        <h1>Join Link Shortener</h1>
+
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <button>Create Account</button>
+        </form>
         <Link to="/">Have an account?</Link>
       </div>
     );
