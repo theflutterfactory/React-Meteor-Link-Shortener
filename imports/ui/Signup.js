59f6603842ec9a51d86ced3a657cbf3ae29e2a14
Julian Currie <julian.currie@gmail.com>
2017-04-07 13:45:35 +0700

    added bcrypt pkg, create user with email & password

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Accounts } from "meteor/accounts-base";
 
 export default class Signup extends React.Component {
   constructor(props) {
@@ -11,6 +12,13 @@ export default class Signup extends React.Component {
 
   onSubmit(e) {
     e.preventDefault();
+
+    let email = this.refs.emailRef.value.trim();
+    let password = this.refs.passwordRef.value.trim();
+
+    Accounts.createUser({ email, password }, err => {
+      console.log("Signup callback", err);
+    });
 
     this.setState({
       error: "Opps..Something went wrong."
@@ -25,8 +33,13 @@ export default class Signup extends React.Component {
         {this.state.error ? <p>{this.state.error}</p> : undefined}
 
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
+          <input type="email" ref="emailRef" name="email" placeholder="Email" />
+          <input
+            type="password"
+            ref="passwordRef"
+            name="password"
+            placeholder="Password"
+          />
           <button>Create Account</button>
         </form>
         <Link to="/">Have an account?</Link>
