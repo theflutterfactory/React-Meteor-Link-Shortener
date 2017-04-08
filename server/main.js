277525a8277c2ce1c83937d1005c1820f7f0b18c
Julian Currie <julian.currie@gmail.com>
2017-04-08 18:36:51 +0700

    Moved user server validation from main.js to new users.js file

--- server/main.js
+++ server/main.js
@@ -1,22 +1,4 @@
 import { Meteor } from "meteor/meteor";
-import SimpleSchema from "simpl-schema";
-import { Accounts } from "meteor/accounts-base";
+import "../imports/api/users";
 
-Meteor.startup(() => {
-  Accounts.validateNewUser(user => {
-    const email = user.emails[0].address;
-
-    try {
-      new SimpleSchema({
-        email: {
-          type: String,
-          regEx: SimpleSchema.RegEx.Email
-        }
-      }).validate({ email });
-    } catch (e) {
-      throw new Meteor.Error(400, e.message);
-    }
-
-    return true;
-  });
-});
+Meteor.startup(() => {});
