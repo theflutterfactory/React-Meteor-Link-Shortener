9d6da252e7dc4d25bac627c43657dfd3beacadc9
Julian Currie <julian.currie@gmail.com>
2017-04-09 20:17:17 +0700

    created button to copy shortUrl using the Clipboard pkg

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,11 +1,36 @@
 import React from "react";
+import ClipBoard from "clipboard";
 
 export default class LinksListItem extends React.Component {
+  componentDidMount() {
+    this.clipboard = new ClipBoard(this.refs.copy);
+
+    this.clipboard
+      .on("success", e => {
+        console.info("Action:", e.action);
+        console.info("Text:", e.text);
+        console.info("Trigger:", e.trigger);
+
+        e.clearSelection();
+      })
+      .on("error", e => {
+        console.error("Action:", e.action);
+        console.error("Trigger:", e.trigger);
+      });
+  }
+
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
+
   render() {
     return (
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>
+          Copy
+        </button>
       </div>
     );
   }
