f764c09b5f9bad0760c312f94e2573a05595ce4b
Julian Currie <julian.currie@gmail.com>
2017-04-09 16:29:13 +0700

    use WebApp to redirect user to link url using the link _id

--- server/main.js
+++ server/main.js
@@ -1,6 +1,21 @@
 import { Meteor } from "meteor/meteor";
+import { WebApp } from "meteor/webapp";
+
 import "../imports/api/users";
-import "../imports/api/links";
+import { LinksApi } from "../imports/api/links";
 import "../imports/startup/simple-schema-config";
 
-Meteor.startup(() => {});
+Meteor.startup(() => {
+  WebApp.connectHandlers.use((req, res, next) => {
+    const _id = req.url.slice(1);
+    const link = LinksApi.findOne({ _id });
+
+    if (link) {
+      res.statusCode = 302;
+      res.setHeader("Location", link.url);
+      res.end();
+    } else {
+      next();
+    }
+  });
+});
