7ccacc8bed52006d2f3befa194cd93b8fd5f9b7f
Julian Currie <julian.currie@gmail.com>
2017-04-10 19:40:32 +0700

    Set an error message in Modal. Focus input field when Modal opens

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -7,7 +7,8 @@ export default class AddLink extends React.Component {
     super(props);
     this.state = {
       url: '',
-      isOpen: false
+      isOpen: false,
+      error: ''
     }
   }
 
@@ -15,13 +16,13 @@ export default class AddLink extends React.Component {
     const url = this.state.url;
     e.preventDefault();
 
-    if (url) {
-      Meteor.call("links.insert", url, (err, res) => {
-        if (!err) {
-          this.setState({ url: '', isOpen: false });
-        }
-      });
-    }
+    Meteor.call("links.insert", url, (err, res) => {
+      if (!err) {
+        this.setState({ url: '', isOpen: false, error: '' });
+      } else {
+        this.setState({ error: err.reason });
+      }
+    });
   }
 
   onChange(e) {
@@ -34,17 +35,22 @@ export default class AddLink extends React.Component {
     return (
       <div>
         <button onClick={() => this.setState({ isOpen: true })}>+ Add Link</button>
-        <Modal isOpen={this.state.isOpen} contentLabel="Add Link">
-          <p>Add Links</p>
+        <Modal
+          isOpen={this.state.isOpen}
+          contentLabel="Add Link"
+          onAfterOpen={() => this.refs.url.focus()}>
+          <h1>Add Links</h1>
+          {this.state.error ? <p>{this.state.error}</p> : undefined}
           <form onSubmit={this.onSubmit.bind(this)}>
             <input
               type="text"
               placeholder="URL"
+              ref="url"
               value={this.state.url}
               onChange={this.onChange.bind(this)} />
             <button>Add Link</button>
           </form>
-          <button onClick={() => this.setState({ isOpen: false, url: '' })}>Cancel</button>
+          <button onClick={() => this.setState({ isOpen: false, url: '', error: '' })}>Cancel</button>
         </Modal>
       </div>
     );
