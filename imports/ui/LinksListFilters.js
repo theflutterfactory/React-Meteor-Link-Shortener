39648992b4f8037841d9faf769d92d56332baf62
Julian Currie <julian.currie@gmail.com>
2017-04-10 17:30:42 +0700

    Track Session state variable so checkbox is always in sync

--- imports/ui/LinksListFilters.js
+++ imports/ui/LinksListFilters.js
@@ -1,17 +1,39 @@
 import React from "react";
 import { Session } from "meteor/session";
 
-export default () => {
-  return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          onChange={e => {
-            Session.set("showVisible", !e.target.checked);
-          }} />
-        show hidden files
+export default class LinksListFilters extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showVisible: true
+    }
+  }
+
+  componentDidMount() {
+    this.linksTracker = Tracker.autorun(() => {
+      this.setState({
+        "showVisible": Session.get("showVisible")
+      })
+    });
+  }
+
+  componentWillUnmount() {
+    this.linksTracker.stop();
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={!this.state.showVisible}
+            onChange={e => {
+              Session.set("showVisible", !e.target.checked);
+            }} />
+          show hidden files
       </label>
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
