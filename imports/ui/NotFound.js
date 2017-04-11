5b27ee4f802b1dba654843f8f4528964724d68b6
Julian Currie <julian.currie@gmail.com>
2017-04-11 20:42:54 +0700

    Added new views and Link to NotFound.js

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from "react-router";
 
 export default () => {
-  return <p>Not Found Component</p>;
+  return (
+    <div className="boxed-view">
+      <div className="boxed-view__box" >
+        <h1>Page Not Found</h1>
+        <p>Hmmm, We were unable to find that page</p>
+        <Link to="/">HEAD HOME</Link>
+      </div>
+    </div>
+  );
 };
